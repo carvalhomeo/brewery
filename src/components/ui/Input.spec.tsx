cef2d01fa75f9cf6d3fbce5745258a21229d27b8
Marcel Carvalho <marcelcarvalho@Marcels-MacBook-Pro.local>
2023-06-23 13:56:59 +0100

    Added test to input component

--- src/components/ui/Input.spec.tsx
+++ src/components/ui/Input.spec.tsx
@@ -1,5 +1,6 @@
 import Input from './Input'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 describe('Input Component', () => {
   it('should render input with label', () => {
@@ -7,5 +8,16 @@ describe('Input Component', () => {
     const input = screen.getByRole('textbox', { name: /name/i })
 
     expect(input).toBeInTheDocument()
+  })
+
+  it('should be able to type', async () => {
+    const user = userEvent.setup()
+    render(<Input type="text" label="Name" name="name" />)
+    const input = screen.getByRole('textbox', { name: /name/i })
+
+    expect(input).toBeInTheDocument()
+    await user.type(input, 'This is my name')
+    expect(input).toHaveValue('This is my name')
+    screen.logTestingPlaygroundURL()
   })
 })
